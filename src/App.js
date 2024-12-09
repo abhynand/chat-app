ac2f11daa5b5e47567301f01bde1add4af528c53
Abhinand M Shibu <abhinandmshibu@gmail.com>
2024-12-09 11:42:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getFirestore, collection, query, orderBy, addDoc, serverTimestamp } from 'firebase/firestore';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJyQ9tEWs94ah3BDZLBnECldZT13zOsRA",
+  authDomain: "chat-app-da365.firebaseapp.com",
+  databaseURL: "https://chat-app-da365-default-rtdb.firebaseio.com",
+  projectId: "chat-app-da365",
+  storageBucket: "chat-app-da365.firebasestorage.app",
+  messagingSenderId: "460082479583",
+  appId: "1:460082479583:web:0fc8505f8d1c3524ec846a",
+  measurementId: "G-MPF5BBWFVV"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Chat App</h1>
+        <SignOut />
+      </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  );
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => signOut(auth)}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = collection(firestore, 'messages');
+  const q = query(messagesRef, orderBy('createdAt', 'asc'));
+
+  const [messages] = useCollectionData(q, { idField: 'id' });
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await addDoc(messagesRef, {
+      text: formValue,
+      createdAt: serverTimestamp(),
+      uid,
+      photoURL
+    });
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+        <button type="submit" disabled={!formValue}>Submit</button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  console.log(photoURL);
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://media.gq-magazine.co.uk/photos/5d13a4349fa60175c8839622/1:1/w_1280,h_1280,c_limit/Ana-de-Armas-GQ-8Jun17_rex_b.jpg'} alt='' />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default App;
